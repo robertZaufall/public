993093af7e6fe98fce7cad01b3fb75ae2a42cb33
robertZaufall <mail@zaufall.de>
2022-01-30 09:02:49 +0100

    Update setup-bdc-20.sh
    update CU version to 15 and kubernetes version to 1.21

--- bdc/setup-bdc-20.sh
+++ bdc/setup-bdc-20.sh
@@ -34,8 +34,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Kube version.
 #
-KUBE_DPKG_VERSION=1.20.12-00
-KUBE_VERSION=1.20.12
+KUBE_DPKG_VERSION=1.21.8-00
+KUBE_VERSION=1.21.8
 
 # Wait for 5 minutes for the cluster to be ready.
 #
@@ -48,7 +48,7 @@ RETRY_INTERVAL=5
 #
 export DOCKER_REGISTRY="mcr.microsoft.com"
 export DOCKER_REPOSITORY="mssql/bdc"
-export DOCKER_TAG="2019-CU14-ubuntu-20.04"
+export DOCKER_TAG="2019-CU15-ubuntu-20.04"
 
 # Variables used for azdata cluster creation.
 #
